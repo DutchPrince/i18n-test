8011f24f1fc7c601c8066895bb74f782cffac09e
DutchPrince <koendegoffau12@gmail.com>
2020-03-04 14:08:14 +0100

    now fix

--- server/i18n.ts
+++ server/i18n.ts
@@ -1,10 +1,16 @@
 // #region Global Imports
 import NextI18Next from "next-i18next";
+import * as common from "@static/locales/en/common.json";
 // #endregion Global Imports
+const path = require("path");
 
 const NextI18NextInstance = new NextI18Next({
     defaultLanguage: "en",
     otherLanguages: ["es", "tr"],
+    resources: {
+        en: { common },
+    },
+    localePath: path.join(__dirname, "static", "locales"),
 });
 
 export const { appWithTranslation, withTranslation } = NextI18NextInstance;
